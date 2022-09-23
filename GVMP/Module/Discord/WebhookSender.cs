4d37560eb70e9b9c6796a6ba03c0ad79c74465b0
NickWoke <104304235+NickWoke@users.noreply.github.com>
2022-09-23 22:52:27 +0200

    added amorroute

--- GVMP/Module/Discord/WebhookSender.cs
+++ GVMP/Module/Discord/WebhookSender.cs
@@ -13,7 +13,7 @@ namespace GVMP
             {
                 DateTime now = DateTime.Now;
                 string[] strArray = new string[20];
-                strArray[0] = "{\"username\":\"VISION. CRIMELIFE.\",\"avatar_url\":\"https://cdn.discordapp.com/attachments/982620830351638538/982629428666458154/server_banner.gif\",\"content\":\"\",\"embeds\":[{\"author\":{\"name\":\"Crimelife.\",\"url\":\"https://discord.gg/tMRXHKWwF2\",\"icon_url\":\"https://cdn.discordapp.com/icons/959806126159962142/a_dbd4efaa0393c50628089928b3059ead.gif?\"},\"title\":\"" + type + "\",\"thumbnail\":{\"url\":\"https://cdn.discordapp.com/icons/959806126159962142/a_dbd4efaa0393c50628089928b3059ead.gif?\"},\"url\":\"https://discord.gg/tMRXHKWwF2\",\"description\":\"Es wurde am **";
+                strArray[0] = "{\"username\":\"VISION. CRIMELIFE.\",\"avatar_url\":\"https://media.discordapp.net/attachments/990695649542557706/1020811999556423710/vision_neu.png?width=641&height=640\",\"content\":\"\",\"embeds\":[{\"author\":{\"name\":\"Crimelife.\",\"url\":\"https://discord.gg/tMRXHKWwF2\",\"icon_url\":\"https://cdn.discordapp.com/icons/959806126159962142/a_dbd4efaa0393c50628089928b3059ead.gif?\"},\"title\":\"" + type + "\",\"thumbnail\":{\"url\":\"https://cdn.discordapp.com/icons/959806126159962142/a_dbd4efaa0393c50628089928b3059ead.gif?\"},\"url\":\"https://discord.gg/tMRXHKWwF2\",\"description\":\"Es wurde am **";
                 int num = now.Day;
                 strArray[1] = num.ToString();
                 strArray[2] = ".";
@@ -32,7 +32,7 @@ namespace GVMP
                 strArray[11] = title;
                 strArray[12] = "\",\"value\":\"";
                 strArray[13] = msg;
-                strArray[14] = "\",\"inline\":true}],\"footer\":{\"text\":\" Vison. Crimelife | " + type + " (c) Sila_Kashlikov 2022\"}}]}";
+                strArray[14] = "\",\"inline\":true}],\"footer\":{\"text\":\" Vison. Crimelife | " + type + " (c) Vision Crimelife 2022\"}}]}";
                 string stringPayload = string.Concat(strArray);
                 StringContent httpContent = new StringContent(stringPayload, Encoding.UTF8, "application/json");
                 using (HttpClient httpClient = new HttpClient())
---
+++ GVMP/Module/Farming/WestenRoute.cs
@@ -0,0 +1,68 @@
+﻿using GTANetworkAPI;
+using Crashkopf.Inventar;
+using Crashkopf.Fraktion;
+using Crashkopf.Player;
+using Crashkopf.Animation;
+using System;
+
+namespace Vision.Routen
+{
+    class WestenRoute : Script
+    {
+        public static Vector3 westenKiste = new Vector3(1012.51, 1626.124, 177.4367);
+        public static bool westeKistenSammeln(Client client, bool pressed)
+        {
+            try
+            {
+                if (client.Position.DistanceTo(westenKiste) <= 25)
+                {
+                    if (pressed)
+                    {
+                        if (client.GetSharedData("isFarming") == false || !client.HasSharedData("isFarming"))
+                        {
+                            int timer = 181;
+                            AnimationDictionary.callAnimation(client, Convert.ToInt32(AnimationDictionary.animationType.leer));
+                            AnimationDictionary.callAnimation(client, Convert.ToInt32(AnimationDictionary.animationType.snowball));
+                            client.TriggerEvent("client:disableinv");
+                            Main.freezePlayer(client, true);
+                            client?.SetSharedData("isFarming", true);
+                            FraktionHandler fraktion = PlayerHelper.getFraktion(client);
+                            switch (fraktion.fraktion)
+                            {
+                                case "zivi":
+                                    client?.TriggerEvent("client:startFarming", "Du hast eine Westenkiste eingesteckt.", 180);
+                                    timer = 180;
+                                    break;
+                                default:
+                                    client?.TriggerEvent("client:startFarming", "Du hast eine Westenkiste eingesteckt.", 181);
+                                    timer = 181;
+                                    break;
+                            }
+                            Item westenkiste = new Westenkiste();
+
+                            NAPI.Task.Run(() =>
+                            {
+                                AnimationDictionary.callAnimation(client, Convert.ToInt32(AnimationDictionary.animationType.leer));
+                                client.TriggerEvent("client:enableinv");
+                                InventorySystem.AddItem(client, westenkiste.Item_Name, 1, westenkiste.maxItemQuantity);
+                                client?.SetSharedData("isFarming", false);
+                                Main.freezePlayer(client, false);
+                            }, delayTime: timer * 1000);
+                            client.TriggerEvent("client:disableinv");
+                            return false;
+
+                        }
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+                return false;
+            }
+            catch { return false; }
+        }
+    }
+}
+
+
